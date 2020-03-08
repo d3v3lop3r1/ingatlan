822159995be12a28f0504257e94818a70b3aa600
d3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-03-08 20:33:27 +0100

    navbar es search javitas

--- resources/views/layout/search.blade.php
+++ resources/views/layout/search.blade.php
@@ -6,22 +6,20 @@
 @endphp
 <div class="container search-container">
     <div class="row">
-        <div class="col-xs-12  drop-shadow search-box">
-            <div class="form-row">
+        <div class="col-xs-12">
+            <div class="form-row  drop-shadow search-box">
                 {!! Form::open(['url'=>'/search', 'method'=>'POST','name'=>'search', 'class'=>'form-inline']); !!}
-                    <div class="form-group col-sm-3">
-                        {!! Form::label('Hirdetés típusa',null,['class' => '', 'for'=>'type_id']); !!}
+                    <div class="col">
+                        {!! Form::label('Típus',null,['class' => '', 'for'=>'type_id']); !!}
                         {!! Form::select('type_id',[
                             0=>'Eladó',
-                            1=>'Eladó cserelehetőséggel',
-                            2=>'Kiadó eladási opcióval',
                             3=>'Kiadó',
                             ],null,['class' => 'form-control form-control-sm', 'id'=>'type_id']); 
                         !!}
                     </div>
 
-                    <div class="form-group col-sm-3">
-                        {!! Form::label('Hirdetes kategória',null,['class' => '', 'for'=>'list_type']); !!}
+                    <div class="col">
+                        {!! Form::label('Ingatlantípus',null,['class' => '', 'for'=>'list_type']); !!}
                         {!! Form::select('list_type',[
                             0=>'Mind',
                             1=>'Lakás',
@@ -39,25 +37,25 @@
                         !!}
                     </div>
 
-                    <div class="form-group col-sm-3">
+                    <div class="col">
                         {!! Form::label('Város',null,['class' => '', 'for'=>'city']); !!}
                         {!! Form::select('city',(['0'=>'Mind']+$cities)
                             ,null, ['class' => 'form-control form-control-sm', 'id'=>'city']);
                          
                         !!}
                     </div>
-                    <div class="form-group col-sm-3">
+                    <div class="col">
                         {!! Form::label('Szobák száma',null,['class' => '', 'for'=>'room_no']); !!}
                         {!! Form::number('room_no',null,['class' => 'form-control form-control-sm', 'id'=>'room_no']); !!}
                     </div>
 
-                    <div class="form-group col-sm-3">
+                    <div class="col">
                         {!! Form::label('Lakóterület(m2)',null,['class' => '', 'for'=>'area']); !!}
                         {!! Form::number('area',null,['class' => 'form-control  form-control-sm', 'id'=>'area']); !!}
                       </div>
 
-                    <div class="col-xs-12 mt-2">
-                        <button class="btn btn-primary" type="submit" >KERESÉS</button>
+                    <div class="col">
+                        <button class="btn btn-primary btn-sm" type="submit" >KERESÉS</button>
                     </div>
                 {!! Form::close() !!}
             </div>
