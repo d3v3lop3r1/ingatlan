9529791092abf26e89b5c465b5dba457d68b031c
Csernus Zoltan <d3v3lop3r1@Csernuss-iMac.local>
2020-03-09 12:54:34 +0100

    berbeado kiado

--- resources/views/layout/newnavbar.blade.php
+++ resources/views/layout/newnavbar.blade.php
@@ -32,7 +32,7 @@
                           </li>
                           <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" 
-                            aria-haspopup="true" aria-expanded="false">Bérbeadó ingatlanok<span class="caret"></span></a> 
+                            aria-haspopup="true" aria-expanded="false">Kiadó ingatlanok<span class="caret"></span></a> 
                             <ul class="dropdown-menu">
                               <li><a href="#"> </a></li>
                               <li><a href="#">Lakás</a></li>
