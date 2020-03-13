ff0725207cd7d1ffb45bee1966df80592b9c438d
Csernus Zoltan <d3v3lop3r1@Csernuss-iMac.local>
2020-03-13 15:41:58 +0100

    kereses kijelzes jav

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -22,14 +22,74 @@ class PagesController extends Controller
         return view('pages.show', compact('properties'));
     }
 
-
     public function search(Request $request)
     {
+        $mutato='KERESÉS/';
         $type_id = $request->type_id;
+        switch($type_id){
+            case 0:
+                $mutato .= 'ELADÓ/';
+            break;
+            case 1:
+                $mutato .= 'ELADÓ CSERELEHETŐSÉGGEL/';
+            break;
+            case 2:
+                $mutato .= 'KIADÓ ELADÁSI OPCIÓVAL';
+                break;
+            case 3:
+                $mutato .= 'KIADÓ/';
+            break;
+        }
+
         $list_type = $request->list_type;
+        switch($list_type){
+            case 0:
+                $mutato .= 'MIND/';
+            break;
+            case 1:
+                $mutato .= 'LAKÁS/';
+            break;
+            case 2:
+                $mutato .= 'HÁZ/';
+            break;
+            case 3:
+                $mutato .= 'NYARALÓ/';
+            break;
+            case 4:
+                $mutato .= 'GARÁZS/';
+            break;
+            case 5:
+                $mutato .= 'IRODA/';
+            break;
+            case 6:
+                $mutato .= 'IPARI/';
+            break;
+            case 7:
+                $mutato .= 'RAKTÁR/';
+            break;
+            case 8:
+                $mutato .= 'ÜZLETHELYISÉG/';
+            break;
+            case 9:
+                $mutato .= 'TELEK-FÖLD/';
+            break;
+            case 10:
+                $mutato .= 'VENDÉGLÁTÁS/';
+            break;
+            case 11:
+                $mutato .= 'EGYÉB/';
+            break;
+                            }
         $city = $request->city;
+        if ($city){
+            $mutato .= strtoupper($city) . '/';
+        }
         $room_no = $request->room_no;
+        if ($room_no){
+            $mutato .= 'SZOBÁK SZÁMA-' . $room_no . '+/';
+        }
         $area = $request->area;
+
         $land_area = $request->land_area;
         $price_min = $request->price_min;
         $price_max = $request->price_max;
@@ -69,9 +129,9 @@ class PagesController extends Controller
 
         ->get();
 
-
+        $prop_count = $properties->count();
         
-        return view('pages.show', compact('properties'));
+        return view('pages.show', compact('properties', 'mutato', 'prop_count'));
     }
 
 
--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -7,124 +7,132 @@ use App\property;
 ?>
 @extends('layout.main')
 @section('tartalom')
-    <section id="ingatlanok-header">
+    @if ($prop_count)
+        <section id="ingatlanok-header">
+            <div class="container-fluid">
+                <div class="row center-xs">
+                    <div class="col-xs-12">
+                        <h1>Ingatlanok</h1>
+                        <h4>{{$mutato}}</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="ingatlanok-lista">
+            <div class="container-fluid">
+                <div class="row center-xs ingatlan-sor">
+                    @foreach ($properties as $property)                       
+                            <?php 
+                            $photo_count = $property->photos->count();
+                            $photo = $property->photos->where('is_default','1')->first();
+                            if ($photo){
+                                $photo_file = $photo->file1;
+                                $photo_file = "uploads/" . $photo_file;
+                            } else {
+                                $photo_file = "placeholder.png";
+                            }
+
+                            ?>
+                            
+                            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ingatlan-box mb-3">
+                                    <div class="row">
+                                        <div class="col-xs-12 prop-header">
+                                            <h5><a class="stretched-link" href="index/{{$property->id}}">{{$property->header}}</a></h5>
+                                            <div class="row justify-content-between">
+                                                <div class="col-md-6 prop-header-alatt-bal start-md ">
+                                                    <?php
+                                                    $dt = Carbon::parse($property->updated_at);
+                                                    $days=$dt->diffInDays();
+                                                    switch($property->type_id){
+                                                        case 0:
+                                                            echo 'Eladó';
+                                                            break;
+                                                        case 1:
+                                                            echo 'Eladó cserelehetőséggel';
+                                                            break;
+                                                        case 2:
+                                                            echo 'Kiadó eladási opcióval';
+                                                            break;
+                                                        case 3:
+                                                            echo 'Kiadó';
+                                                            break;
+                                                    }
+                                                    if ($days < 14){
+                                                        echo "<span class='badge badge-secondary badge-danger ml-2'>Új</span>";
+                                                    }
+                                                    ?>
+                                                </div>
+                                                <div class="col-md-2 prop-header-alatt-jobb">
+                                                    <i class="fas fa-camera-retro"></i> {{$photo_count}}
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="col-xs-12 main-photo">
+                                            <img class="img-thumbnail" src="{{$photo_file}}" alt="">
+                                        </div>
+                                    </div>
+
+
+                                    {{--  Adatok  --}}
+                                    
+                                    
+                                    <div class="row center-xs">
+                                        <div class="col-xs-12 main-price border">
+                                            <span class="money">{{$property->price}}</span>.- Ft
+                                        </div>
+                                        <div class="col-xs-12 main-details">
+                                            <div class="row kiskockak center-xs border">
+                                                <div class="col xs-4">
+                                                    <i class="fa fa-ruler fa-xl"></i><span> Terület:</span><br>
+                                                    @if ($property->land_area)
+                                                        {{$property->land_area}}m2
+                                                    @else
+                                                        -
+                                                    @endif
+                                                </div>
+                                                <div class="col xs-4">
+                                                    <i class="fas fa-expand-alt fa-xl"></i><span> Lakótér:</span><br>
+                                                    @if ($property->area)
+                                                        {{$property->area}}m2
+                                                    @else
+                                                        -
+                                                    @endif
+                                                </div>
+                                                <div class="col xs-4">
+                                                    <i class="fas fa-th-large fa-xl"></i><span> Szobák:</span><br>
+                                                    @if ($property->room_no)
+                                                        {{$property->room_no}}
+                                                    @else
+                                                        -
+                                                    @endif
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>                                                   
+                            </div>
+                    @endforeach
+                    
+                </div>
+            </div>
+            <div class="container lapozas">
+                <div class="row">
+                    <div class="col-xs mx-auto">
+                        {{-- <p>
+                            {{ $properties->links() }}
+                        </p> --}}
+                    </div>
+                </div>
+            </div>
+        </section>
+    @else
         <div class="container-fluid">
             <div class="row center-xs">
                 <div class="col-xs-12">
-                    <h1>Ingatlanok</>
+                    <h3 class="text-danger">SAJNOS NINCS A KERESÉSNEK MEGFELELŐ INGATLAN!</h3>
                 </div>
             </div>
         </div>
-    </section>
-    <section id="ingatlanok-lista">
-        <div class="container-fluid">
-            <div class="row center-xs ingatlan-sor">
-                @foreach ($properties as $property)                       
-                        <?php 
-                        $photo_count = $property->photos->count();
-                        $photo = $property->photos->where('is_default','1')->first();
-                        if ($photo){
-                            $photo_file = $photo->file1;
-                            $photo_file = "uploads/" . $photo_file;
-                        } else {
-                            $photo_file = "placeholder.png";
-                        }
-
-                        ?>
-                        
-                        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ingatlan-box mb-3">
-                                <div class="row">
-                                    <div class="col-xs-12 prop-header">
-                                        <h5><a class="stretched-link" href="index/{{$property->id}}">{{$property->header}}</a></h5>
-                                        <div class="row justify-content-between">
-                                            <div class="col-md-6 prop-header-alatt-bal start-md ">
-                                                <?php
-                                                $dt = Carbon::parse($property->updated_at);
-                                                $days=$dt->diffInDays();
-                                                switch($property->type_id){
-                                                    case 0:
-                                                        echo 'Eladó';
-                                                        break;
-                                                    case 1:
-                                                        echo 'Eladó cserelehetőséggel';
-                                                        break;
-                                                    case 2:
-                                                        echo 'Kiadó eladási opcióval';
-                                                        break;
-                                                    case 3:
-                                                        echo 'Kiadó';
-                                                        break;
-                                                }
-                                                if ($days < 14){
-                                                    echo "<span class='badge badge-secondary badge-danger ml-2'>Új</span>";
-                                                }
-                                                ?>
-                                            </div>
-                                            <div class="col-md-2 prop-header-alatt-jobb">
-                                                <i class="fas fa-camera-retro"></i> {{$photo_count}}
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div class="col-xs-12 main-photo">
-                                        <img class="img-thumbnail" src="{{$photo_file}}" alt="">
-                                    </div>
-                                </div>
-
-
-                                {{--  Adatok  --}}
-                                
-                                
-                                <div class="row center-xs">
-                                    <div class="col-xs-12 main-price border">
-                                        <span class="money">{{$property->price}}</span>.- Ft
-                                    </div>
-                                    <div class="col-xs-12 main-details">
-                                        <div class="row kiskockak center-xs border">
-                                            <div class="col xs-4">
-                                                <i class="fa fa-ruler fa-xl"></i><span> Terület:</span><br>
-                                                @if ($property->land_area)
-                                                    {{$property->land_area}}m2
-                                                @else
-                                                    -
-                                                @endif
-                                            </div>
-                                            <div class="col xs-4">
-                                                <i class="fas fa-expand-alt fa-xl"></i><span> Lakótér:</span><br>
-                                                @if ($property->area)
-                                                    {{$property->area}}m2
-                                                @else
-                                                    -
-                                                @endif
-                                            </div>
-                                            <div class="col xs-4">
-                                                <i class="fas fa-th-large fa-xl"></i><span> Szobák:</span><br>
-                                                @if ($property->room_no)
-                                                    {{$property->room_no}}
-                                                @else
-                                                    -
-                                                @endif
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                </div>                                                   
-                        </div>
-                @endforeach
-                
-            </div>
-        </div>
-        <div class="container lapozas">
-            <div class="row">
-                <div class="col-xs mx-auto">
-                    <p>
-                        {{-- @php
-                                
-                        @endphp
-                        {{ $properties->links() }} --}}
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section>
+    @endif
 @endsection
