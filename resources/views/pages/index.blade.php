1c5323fe886a153a06e4414d5b9a5ec1d3976730
Csernus Zoltan <d3v3lop3r1@Csernuss-iMac.local>
2020-03-28 18:06:23 +0100

    index google maps street

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -221,7 +221,7 @@ use Carbon\Carbon;
                       <iframe
                         frameborder="0" style="border:0; width:100%; height:300px;"
                         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBrdH7bXJbDwNHSyPVwWIoyIeTdiUbWP_c
-                        &q={{$property->city,$property->street}}" allowfullscreen > 
+                        &q={{$property->city}}%2C%20{{$property->street}}" allowfullscreen > 
                       </iframe>
                     </div>
                                   <h4>Ingatlant venne?<br>
