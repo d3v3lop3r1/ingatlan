2b7d56982251e3026178303ba5e09a3719372005
Csernus Zoltan <d3v3lop3r1@Csernuss-iMac.local>
2020-03-30 11:19:23 +0200

    navbar koltozes delete

--- resources/views/layout/newnavbar.blade.php
+++ resources/views/layout/newnavbar.blade.php
@@ -59,7 +59,7 @@
                               <li><i class="fas fa-money-bill-wave"></i><a href="/vevoknek-berloknek"> Vevőknek-Bérlőknek</a></li>
                               <li><i class="fas fa-tools"></i><a href="/ingatlanfox-a-gondnok"> Ingatlanfox a Gondnok</a></li>
                               <li><i class="fas fa-hammer"></i><a href="/felujitasok-atepitesek-kivitelezese"> Felújítások-Átépítések kivitelezése</a></li>
-                              <li><i class="fa fa-truck fa-sm"></i><a href="/koltozes-szervezese"> Költözés szervezése</a></li>
+                              {{--  <li><i class="fa fa-truck fa-sm"></i><a href="/koltozes-szervezese"> Költözés szervezése</a></li>  --}}
                               <li><i class="fa fa-question-circle fa-sm"></i><a href="/segitseg-a-haz-korul"> Segítség a ház körül</a></li>
                             </ul>
                           </li>
