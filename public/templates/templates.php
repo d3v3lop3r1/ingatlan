e99d2db722cbd3d334535853dbd19f5bc523ed5a
Csernus Zoltán <d3v3lop3r1@Csernuss-iMac.local>
2020-04-03 19:25:13 +0200

    templates json jav

--- public/templates/templates.php
+++ public/templates/templates.php
@@ -1,5 +1,5 @@
 [
   {"title": "Magyar ház temp", "description": "Magyar ház template, teljesen kitöltve", "url": "/templates/hu_haz.php"},
-  {"title": "Német ház temp", "description": "Német ház template, teljesen kitöltve", "url": "/templates/de_haz.php"}
+  {"title": "Német ház temp", "description": "Német ház template, teljesen kitöltve", "url": "/templates/de_haz.php"},
   {"title": ".HU összkomfortos temp", "description": "Magyar nyelvű összkomfortos temp", "url": "/templates/hu_osszkomfortos.php"}
 ]
