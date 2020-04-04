c80d64c47986990b72c7eceabbc4edb0717b7700
Csernus Zoltán <d3v3lop3r1@Csernuss-iMac.local>
2020-04-04 02:17:32 +0200

    templates add more

---
+++ public/templates/de_raktar_kiado.php
@@ -0,0 +1,32 @@
+Lagerhalle zu vermieten in Pécs, Stadteil Süd-Osten auf der Mohácsi út. <br>
+<br>
+Der Lager ist auf einem Gewerbehof, wo auch große LKW einfahren kann.<br>
+<br>
+<h4>Raumaufteilung – Ausstauttung</h4>
+Gesamtfläche von Lagermöglichkeit: 500 m2, höhe 4-5 m<br>
+Boden: stabil betoniert<br>
+Größe vom Eigangstür zum Lagerfläche: 300x400 cm<br>
+Sonstige nutzbare Räumlichkeiten: Büro 15 m2, WC, Duschraum<br>
+Nutzflächen am Hof: <br>
+Wasser, Strom, 360 Volt Hochspannung, Internetanschluss möglich<br>
+<br>
+Bérleti szerződés - díjfizetések<br>
+<br>
+Vertragsdauer min 1 Jahr<br>
+Mietangebot 90.000 Ft / Monat kalt, zahlbar bis je 3. des Monats <br>
+Kaution 2 Monatsmiete – hinterlegen beim Vertragsabschluss<br>
+<br>
+Entfernungen<br>
+<br>
+Bushaltestelle 400 m<br>
+Autobahn 3,5 km (M6)<br>
+Lebensmittelgeschäft 700 m <br>
+Zentrum der Ortschaft 2 km, Pécs<br>
+<br>
+Verfügbar<br>
+<br>
+ab sofort, oder in 30 Tagen<br>
+<br>
+
+
+<small>Bezüglich externe Datenangaben für die genauigkeiten der Daten übernehmen wir keine Haftung. Wir behalten uns das Recht eventuelle Preisänderungen durchzuführen.</small> 
---
+++ public/templates/hu_raktar_kiado.php
@@ -0,0 +1,33 @@
+Raktárhelyiség kiadó a dél-keleti városrészében a Mohácsi úton. <br>
+<br>
+A raktárhelyiség egy közös ipari ingatlanudvarban van, kamionnal is behajtható a telekre. A raktáringatlan egyszintes, rendelkezik még egy irodahelyiséggel, WC/Mosdóval.<br>
+<br>
+<h4>Térelosztások – felszereltség</h4>
+
+A raktárhelyiség méretei: 500 m2/magasság<br>
+Talapzat: stabil betonozott<br>
+Raktár bejárati ajtó mérete: 300 x 400 cm<br>
+Mellékhelyiségek: Iroda, WC, mosdó, tusoló<br>
+Használható udvar:<br>
+Víz, villany, 360 Volt nagyfeszültség, Internetkapcsolat<br>
+Bérleti szerződés - díjfizetések<br>
+
+Szerződés min. 1 évre<br>
+Bérleti díj 90.000 Ft / hó, fizetve minden hó 3.-áig előre<br>
+Kaució 3 havi bérleti díj<br>
+
+<h4>Távolságok az ingatlantól</h4>
+
+Buszmegálló 400 m<br>
+Autópálya 3,5 km (M6)<br>
+Élelmiszerbolt 700 m<br>
+Település központja 2 km<br>
+<br>
+Beköltözhető<br>
+<br>
+azonnal vagy 30 napon belül<br>
+
+<br>
+
+<small><strong>Adatfelvétel</strong></small> 
+<small>A felsorolt adatok részben a megbízó által felsorakoztatva, így azok pontosságáért nem vállalunk felelősséget. Az árajánlat változtatási jogát fenntartjuk.</small>  
