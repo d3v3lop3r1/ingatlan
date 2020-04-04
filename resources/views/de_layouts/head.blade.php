4440efbd1354794a9470bbe96b30e0179a271ffd
d3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-04-04 12:56:56 +0200

    csrf token to head

--- resources/views/de_layouts/head.blade.php
+++ resources/views/de_layouts/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset='utf-8'>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="/images/logos/ingatlanfox_favicon.png" type="image/x-icon"/>
 <link rel="shortcut icon" href="images/logos/ingatlanfox_favicon.png" type="image/x-icon"/>
 <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina" rel="stylesheet">
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,6 +1,6 @@
-<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset='utf-8'>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8;">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="/images/logos/ingatlanfox_favicon.png" type="image/x-icon"/>
 <link rel="shortcut icon" href="images/logos/ingatlanfox_favicon.png" type="image/x-icon"/>
 <title>Ingatlanfox a transzparens ingatlanügynök</title>
