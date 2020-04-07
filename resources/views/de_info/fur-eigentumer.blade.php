7274a7f74c85cd662d0979385d07ad1c6e4cd875
d3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-04-07 09:16:23 +0200

    tulajdonosoknak info de hu

--- resources/views/de_info/fur-eigentumer.blade.php
+++ resources/views/de_info/fur-eigentumer.blade.php
@@ -5,7 +5,7 @@
     <h2>FÜR EIGENTÜMER</h2>
     <h3>Wie kann ich am schnellsten meine Immobilie in Ungarn verkaufen?</h3>
     <h4>
-        Also allein wird schwerig. Ohne Sprache, wenn Sie kein ungarisch sprechen, mit fehlenden Fach- und Lokalkenntnisse, wie mann sagt leider, "es wird schon schiefgehen".
+        Also allein wird es schwerig. Wenn Sie kein ungarisch sprechen, mit fehlenden Fach- und Lokalkenntnisse, wie mann sagt leider, "es wird schon schiefgehen".
     </h4>
     <p>
         Markwert, Verkehrswert in Südungarn, den Zustand der Immobilie müssen Sie hineinbeziehen
@@ -13,44 +13,21 @@
         Bei Fehlberechnungen laufen Sie die Gefahr Ihr Preisangebot zu billeg oder zu teuer zu setzen.
         Wegen mangelnde Sprachkenntnisse zwischen Ihnen und Ihrem Käufer entstehen Konflikte.
         Bis Sie Ihr Anwesen verkauft haben könnten Sie 10-20 oder mehr Besichtigungen auch anhalten.
-        All diese eventuelle Unreinheiten, Unannähmlichkeiten können Sie sich ersparen
-        wenn Sie uns beauftragen mit dem Verkauf oder mit der Vermietung.
+        Ersparen Sie sich All diese lästige Unannähmlichkeiten
+
     </p>
     <p>
-        Nehmen Sie unsere umfangreiche Dienstleistungen und Ingatlanfox befreit Sie von dieser Sorgen.
-        Sie brauchen um nichst zu kümmern, wir übernehmen alle Aufgaben. Auf einem speciellen Arbeitsblatt notieren will alle Geschehen was Sie auch zum Einsicht bekommen.
-    </p>
-    <h4>
-        Der freundliche Repräsentant von ingatlanfox ist mehr als ein Makler
-    </h4>
-    <p>
-        Informationvergabe lokalen Möglichkeiten
-        Raten- oder mal Abraten in Ihrem Interesse
-        Unser Agent weiß immer, es geht um Ihren Immobilie, um den bestmöglichen Preis zu erzielen
+        WIR VEKAUFEN SCHNELL IHRE IMMOBILIEN!
+
     </p>
     <p>
         In Ungarn - laut Gesetz - Käufer zahlen kein Vermittlungsgebühr
-        Alle Vermittlungskosten, Maklerprovision zahlt der Verkäufer nach Erfolgreichen Verkauf
-    </p>
-    <p>
-    </p>
-    <h3>Bei uns Mal zahlbar nur 1,9 % Erfolgsprovision</h3>
-    <h4>Ja möglich, abhängig vom Auftrags- Art und Dauer, sowie von Angebotspreis</h4>
-    <p>
-        Auftrags Art: ob sie uns ein Exklusivrecht einräumen oder nicht
-        Aufrtrags Dauer: ob der Auftrag für 3-6- oder 12 Monaten 
-        Staffelung nach Angebotspreis, je höher um so weniger Prozent wird berechnet
-        Unsere Reprasentanten führen bei die Liste, sie werden ausführlich informiert
-    </p>
-    <p>
-    </p>
-    <h4></h4>
-    <p>
+        Alle Vermittlungskosten, Maklerprovision zahlt der Verkäufer nach Erfolgreichen Verkauf. Weitere Informationen erfahren bei der Kontaktaufnahme.
     </p>
     <h4>Wie kann ich gratis Annoncieren auf 4-5 Portalen, in mehreren Sprachen, in 3 Länder?</h4>
     <p>
-        Wenn Sie Einer der Ingatlanfox Repräsentanten beuftragen um Ihre Immobilie zu verkaufen, oder zu vermieten.
-    </p>  
+        Wenn Sie Ingatlanfox beauftragen!
+    </p>
     <h4>Wann müsste ich Erfolgsprovision zahlen?</h4>
     <p>
         Erst nach erfolgreichem Verkauf Ihrer Immobilie.
@@ -59,25 +36,7 @@
     <p>
         Ja, allerdings nicht Alle Repräsentanten sprechen ausreichend deutsch.
         Die in der Zentrale immer, also wählen Sie am Anfang entweder 0049-1578-4001214<strong>oder Mobil in Ungarn</strong>0036-20-7772000<strong></strong>  illetve
-        0036-20-7772000 Frau Speck oder Herr Sztarovics informieren Sie gern<strong></strong> 
+        0036-20-7772000 Frau Speck oder Herr Sztarovics informieren Sie gern<strong></strong>
     </p>
-    <hr>
-    <h3>Tipps, Infók für Eigentümer in Ungarn</h3>
-    <h4>Wie viel Wert hat meine Immobilie in Ungarn?</h4>
-    <p>
-        Einer der bekannten Webkalkulator gibbt Einblicke, in ungarischen Sprache, der portfolio.hu
-        
-        <br>
-        <a href="http://www.portfolio.hu/ingatlan/kalkulator.php">Link</a>
-    </p>
-    <h4>Info - wie man preiwert verschönern kann</h4>
-    <p>
-        <a href="http://lakbermagazin.hu/epiteszet-hirek/2243-hovisszavero-tetocserep-vilagszintu-innovacio-a-tetofedesben.html">Link</a>
-    </p>  
-        
-    
-    
-    
-    
-</div>      
+</div>
 @endsection
--- resources/views/info/tulajdonosoknak.blade.php
+++ resources/views/info/tulajdonosoknak.blade.php
@@ -3,110 +3,42 @@
 @section('tartalom')
     <div class="container">
         <h2>TULAJDONOSOKNAK</h2>
-        <h3>Mi lehet ha magam árulom ingatlanom?</h3>
-        <h4>
-            Tudatlanságból eredő veszteségek?
-        </h4>
         <p>
-            Amikor nem tudod ingatlanod Piaci- és Forgalmi értékét aktuálisan kiszámolni, irreális
-            árajánlattal rukkolsz elő, aztán csodálkozol, hogy nem jönnek a vevők, avagy túl olcsón
-            kínálod fel. Ingatlanfox csapatunk ezeket a felméréseket is elvégzi analizálva részletesen.
-            Amikor úgy gondolod egy olcsó árajánlattal sikeres leszel, elhallgatva ezt-azt. Pedig ha a
-            „zsákbamacska“ kibújna az ingatlanszemlék alatt, akkor minden ár magas lehet. Mi a
-            teendő? Azt bízd a profikra, ügynökeink tudják miként legyen ajánlatod transzparens, de
-            méges elfogadható.
-        </p>
-        <p>
-            Van időd minden nap készelétben állni az érdeklődőknek? Tudod mit-hogyan válaszolj
-            kérdéseikre? Van idő a hivatalos intézkedésekre, és tudod is hogyan-mit-mikor kell intézni?
+            Az Ön ajánlatából egy karakteresen megnyerőe bemutatást faragunk
+            fotózás-videózás-drónozás anyagának feldolgozásával.
+            Értesítjük előjegyzésben lévő érdeklődőinket, partnereinket,
+            ajánlatunkat hírdetjük
+            2 nyelven
+            3 országban
+            4-5 ingatlanportálon, az Ön érdekében, egy gyors értékesítésért
         </p>
         <h4>
-            Mi egy megbízható ingatlanost megbízásának előnye?
+            Mi mindenre van szükség egy eredményes eladáshoz?
         </h4>
         <p>
-            Tanácsadások, aktuális hasznos információk mint az eladó úgy a vevő számára is, hiszen a
-            közvetítés lényege, hogy végre összejöjjön az az adás-vétel mindkettőjük megelégedésével.
-            Pénzügyi tanácsadásokkal, analízisikkel, Exposéval megkönnyíti a megbízott ingatlanos a
-            vevő döntését, hogy az adás-vétel kimenetele számára is áttekinthető és meggyőző legyen.
-            Mikor kell fizetnem azt a 3,9% adót, mikor nem?
+            Ingatlanügyviteli szakértelem a problémamentes kivitelezésekhez!
+            Jó kapcsolatok, partnerekkel, előjegyzésben lévő fizetőképes érdeklődőkkel!
+            Sok idő érdeklődőkkel ingatlanszemlékre való kiutazásokhoz
+            Adás-vételi megállapodások szakszerű kivitelezése
+            Költségek megelőlegezése rizikóval, hiszen csak sikeres értékesítés lesz Öntől díjazva.
         </p>
         <p>
-            Egy ingatlaneladásnak számtalan állomása, hivatalokban történő intézkedése van, amikben
+            Egy ingatlaneladásnak számtalan állomása, hivatali intézkedése van, amikben
             nem ajánlatos tévedni,, mert az zsebre megy...
         </p>
+        <h4>Mik azok az esetek amikor a vevőmnek nem tudok eleget tenni?</h4>
         <p>
-            Apropo, azt a 2-3% sikerjutalékot meg ingatlanosom visszahozhatja nekem top-analíziseivel,
-            bemutatásával, stb. ügyeskedéssel, jobb értékesítéssel mint vártam volna. 
+            Sok esetben szükség van professzionális prezentálásra
+            Exposé készítésére, olykor Steakholder analízisre
+            amit már számtalan ügynökség bevezetett
+            a transzparens, igen csak részletes bemutatás érdekében.
+            Külföldi érdeklődők esetében mi gondoskodunk tolmács felől.
+            Amikor Ön dolgozik vagy éppen külföldön van, mi ott lehetünk vevőjével ingatlanszemléhez.
         </p>
-        <h3>Miért kellhet nekem az Ingatlanfox segítsége?</h3>
-        <h4>Miért van több esélye ingatlana értékesítésére velünk?</h4>
+        <h4>Ingyenes hirdetések?</h4>
         <p>
-            Ingatlanát nam csak az Ingatlanfox oldalain, hanem más magyar és külföldi
-            portálokon is hírdetjük attraktív bemutatással. Magyar és német ajkú érdeklődőit
-            anyanyelvén világosítjuk fel. Német ingatlanügynöki partnerünk révén is folyamatosan
-            érkeznek érdeklődők, akik Baranya megyében szeretnének ingatlant vásárolni.
-            A külföldi ingatlanportálokról jelentkezőket, illetve a német anyanyelvű érdeklődőket az
-            Ingatlanfox ügynökei informálják, kérésükre az Ön ingatlana Exposéját, Steakholder
-            analízisét elkészítik, átadják, egy meggyőzőbb bemutatás érdekében. Ingatlanszemléket
-            tartanak, tolmácsolnak ha kell.
+            Az Ingatlanfox oldalakon csak Eladási- vagy Kiadási Megbízás után lehet hirdettetni.
         </p>
-        <p>
-            Ingatlanszemléken az érdeklődőkkel ügynökeink tudják mit-mikor-hogyan szóljanak közbe. A
-            tulajdonosok ebben szinte mindig tévednek...
-        </p>
-        <h4>Hol lesz látható az ingatlanom?</h4>
-        <p>
-            Az Ön ajánlatából egy karakteres, megnyerően mutatós bemutatást faragunk a legújabb
-            technológiák, fotómontázsok, szükség esetén drón bevetésével. Ajánlata 2 nyelven
-            (magyarul és németül), 3 országban, és 4+ ingatlanos portálokon jelentetjük meg! Előjegyzett
-            érdeklődőink folyamatosan értesítjük amennyiben az Ön ingatlanához hasonló keresnek.
-        </p>
-        <h4>Hogyan adhatok fel hírdetést az Ingatlanfoxon?</h4>
-        <p>
-            Az Ingatlanfox oldalakon csak Megbízás után lehet hirdetni ingyenesen. Ehhez válasszon
-            tetszés szerint egy képviselőnket, vagy üzenjen nekünk az info@ingatlanfox.hu Emailre, vagy
-            hívjon a <strong>0036-20-777 2000 mobilszámon (Németországból a 0049-157-8400 1214 számon)</strong>. <br>
-            Üzenetére szívesen válaszolunk, telefonszáma megadása esetén felhívjuk! Megbízása után
-            nem csak az Ingatlanfox oldalain, de más belföldi és külföldi portálokon is megjelentetjük
-            ajánlatát a gyors siker érdekében.
-        </p>  
-        <h4>Kell e előre fizetnem?</h4>
-        <p>
-            Nem! Nem, mert mi bízunk a mielőbbi sikeres értékesítésben, amihez a kezdetben Öntől csak
-            egy kis kooperációs segítséget várunk el az adatfeldolgozás, fotózás-filmezés
-            együttműködésére. A többi a mi munkánk lesz, melyet motiváltan vállalunk megbízása
-            szerint. Az Ön által választott képviselő az Ingatlanfox központjával erős háttérrel bír, az
-            ügyvitelek gyorsan és szakszerű fogja elvégezni.
-        </p>
-        <h4>Mi az a sikerjutalék?</h4>
-        <p>
-            Az a jutalék amely csak egy sikeres értékesítés (eladás-csere-bérbeadás-stb) után esedékes
-            törlesztésre. Eladásnál, béerbeadásnál az ingatlanügynök teljeskörű szolgáltatást kell adjon
-            a tanácsadástól, a fotózás-videózáson túl a szerződtetésekig. Itt megjegyezhetjük büszkén,
-            hogy ingatlana bemutatását a legattraktívabbak közé sorolják a többországbani és
-            többnyelvű Marketing végett is. Amennyiben sikeres lesz az eladás- vagy bérbeadás tartozik
-            csak megbízója egy sikerjutalék fizetésével bevétele után. Ennek mértéke az Ingatlanfoxnál
-            példamutatóan fair és alacsony jelenleg! Eladásnál az ingatlan árajánlata- és a megbízás
-            fajtájától függően már vállaljuk 1,9%-tól. Részletesebbet a témához képviselőinktől tudhat
-            meg.
-        </p>
-        <h4>Sikerjutalék</h4>
-        <p>
-            Csakis egy sikeres értékesítés után, illetve az eladási -/vagy bérleti öszzeg átvétele után van
-            elszámolási kötelezettsége megbízott eladójával, ha az bizonyítani is tudja, hogy a vevő-
-            /vagy bérlő az ő érdeklődőiből származik. A sikerjutalék mértéke a megbízási szerződés
-            fajtája (kizárólagos- vagy un. nemkizárólagos jogú), továbbá az ingatlan értéke és a
-            megbízás időtartamának függvénye. Eszerint az Ingatlanfox rendkívül kedvezően, már 1,9%
-            jutalékért, és bérleadásnál esetekben már félhavi bérleti díjért is szolgáltat az Ön számára
-            sikerrel. További részleteket a képviselőktől kaphat ezirányban.
-        </p>
-        <h4>Választhatok kivel szeretném a személyes kapcsolatot?</h4>
-        <p>
-            Igen, az Öné a választás melyik képviselőnkkel szeretne kapcsolatra lépni, vagy hívhatja a
-            központi Mobiljainkat is tanácsadásért <strong>06-20-777 2000</strong>  vagy <strong>06-20-422 0000</strong>  illetve
-            Németországból a <strong>0157-8400 1214</strong> számokat. 
-        </p>
-        <hr>
         <h3>További értékes infók tulajdonosoknak</h3>
         <h4>Mennyit ér az ingatlanom?</h4>
         <p>
@@ -118,11 +50,11 @@
         <h4>Tippek, hogyan tegyem ingatlanom vonzóbbá</h4>
         <p>
             <a href="http://lakbermagazin.hu/epiteszet-hirek/2243-hovisszavero-tetocserep-vilagszintu-innovacio-a-tetofedesben.html">Link a Lakbérmagazin oldalára</a>
-        </p>  
-            
-        
-        
-        
-        
-  </div>      
+        </p>
+
+
+
+
+
+  </div>
 @endsection
