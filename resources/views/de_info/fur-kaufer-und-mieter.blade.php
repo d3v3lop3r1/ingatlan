6c5f0a2e05a383c008208d31d02d81852aba8cfa
d3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-04-08 09:39:53 +0200

    fur kaufer und mieter new

--- resources/views/de_info/fur-kaufer-und-mieter.blade.php
+++ resources/views/de_info/fur-kaufer-und-mieter.blade.php
@@ -4,34 +4,34 @@
 <div class="container">
     <h3>Für Käufer und Mieter</h3>
     <p class="text-muted">
-        Ingatlanfox hilft mit Rat und Tat vor dem Kauf, damit Sie keine falsche Entscheidung treffen. Worauf sollten Sie achten? Vor allem definieren Sie Ihre Erwartungen und Kompromisse. 
-        Seien Sie Lokal gut informiert.
+        Ingatlanfox hilft Ihnen mit Rat und Tat damit Sie keine falsche Entscheidung treffen.
+        Überlassen Sie unsere geschulten Repräsentanten Ihre Trauimmobilien zu finden, die Kaufabwicklung reibungslos zu erledigen.
     </p>
     <p class="leader">
-        Schritt 1: wenn interessantes Angebot gefunden, rufen Sie den ANbieter an, stellen Sie Ihre Fragen
-        Schritt 2: wenn bis jetzt gute Aussichten, rufen Sie den Anbieter an, stellen Sie Ihre Fragen, vereinbaren Sie ein Besichtigungstermin 
-        Schritt 3: listen Sie auf einem Blatt auf, im links-rechts Bloks, die Erwartungen/Wünsche, und die Kompromisse auf
-        Schritt 4: nach Ankunft lassen Sie sich ertsmal für 10-15 Minuten verlieben/mitreißen um das Objekt wenns gefällt (oder auch nicht), am besten nehmen Sie jemand mit der mehr Ahnung hat um die Gebäudebeurteilung...    
-        Schritt 5: Allerdings machen Sie keine negatíve Anmerkungen vorerst, und lassen liber den Verkäufer reden. Fragen Sie jetzt noch keines Wegs um den Preis.
-        Schritt 6: Jetzt aber soll konkret ums Zustand und der Detaille rübergehen. Keine unnötige, inkompetente, oder irrelevante Fehlerausweisungen machen, liber skeptisch "durchwühlen".
-        Schritt 7: Weenn möglich Paar fotos schießen, bitten um Einsicht in alle mögliche Dokumente des Objekts, aber treffen Sie keine Entscheidungen auf Anfrage!
-        Schritt 8: dann freundlich sich verabschieden mit der Bemerkung, "ich melde mich in den Tagen mit meiner Antwort. 
-        Schritt 9: wenn am nächsten Tag fühle, dass mir diese Immobilie optimal wäre, fasse ich kurz schriftlich zusammen was ich negativ fand, und warum biete ich mein Angebot zB. statt 32.000 nur 30 tausend an. Kann auch als Email gesendet werden.
-        Schritt 10: kriege ich keinen Antwort in 24 Stunden, dann anrufen, verhandeln.
-        Mit der Methode liegen Sie richtig in Ungarn, selbst wenn viele "einige Stationen weglassen".
+        <strong>Was zu tun wenn interesse besteht?</strong>
+        Wenn Ihnen ein Angebot gefällt, Anrufen oder Emailen: <strong>info@ingatlanfox.hu</strong>
+        Am besten schreiben Sie uns was für eine Immobilie Sie suchen mit der Angeaben Ihrer Erwartungen, Wünsche, Vorstellungen. <br>
+        Wir suchen Sie wie gewünscht auch bei unseren vorgemerkten Kunden und Partnern, und melden uns bei Ihnen schnell. <br>
+        Es ist üblich, dass man mehrere Objekte besichtigen möchte. <br>
+        Für die Tage bieten wir kostenlose Unterkunft, ein 2 Zimmer Wohnung direkt bei uns in Harkány an. Gerne warten wir auf Sie! <br>
+        <br>
+        Unsere freundlich umfangreiche Dienstleistungen werden auch Sie überzeugen. Wir beraten Sie, bzw. Raten und Abraten Mal in Ihrem Interesse. <br>
+        Sollten Sie anderweitig was Interessantes gefunden haben, können Sie trotzdem unsere Hilfe beanspruchen. Wir helfen damit die Abwicklung ordentlich verläuft. <br>
+        <br>
+        Alle Erwerbskosten sind wesentlich günstiger als in Deutschland: Erwerbsteuer nur 3,95%, Eintrag ins Register/Umschreibung um die 100 Euro, Notar/Anwaltskosten um die 1-1,5%.
     </p>
     <h4>
-        Sie fanden auf andere Portalen die passende Immobilie, aber brauchen Rat, den vertrauenswürdiger Beistand für eine sichere Kaufabwicklung? Fragen Sie uns. Wir helfen!
+        Bei uns Käufer zahlen KEINE PROVISION!
     </h4>
     <hr>
 
     <h4>Finanzierungs-Kalkulator beim Grundstück und Land</h4>
-    
+
     <a href="https://hitelnet.hu/hitelkalkulator/telekvasarlas/"> <strong>LINK</strong></a>
     <hr>
-    <h4></h4> 
+    <h4></h4>
     <p> </p>
-    <p> Supergünstige Finanzierungsmöglichkeiten für junge ungarische Familien "falusi CSOK" Liste der Ortschaften, wo der Staat 100% der Kaufpreis übernimmt, wenn die Familie sich verpflichtet... <a href="https://hitelforum.hu/ezekre-a-telepulesekre-koltozhetsz-ingyen-juliustol-a-kormany-fizet-mindent-itt-a-teljes-telepules-lista/?fbclid=IwAR1E5y5VpFHq9BINF5vifah-JK2SYQC4WDV54Ip5jSLTSjjeBdf3g9ROKXc"> LINK</a></p>
+    <p> Supergünstige Finanzierungsmöglichkeiten für junge ungarische Familien "falusi CSOK" Liste der Ortschaften, wo der Staat den Kaupreis bis zu 100% übernimmt, wenn die Familie sich verpflichtet... <a href="https://hitelforum.hu/ezekre-a-telepulesekre-koltozhetsz-ingyen-juliustol-a-kormany-fizet-mindent-itt-a-teljes-telepules-lista/?fbclid=IwAR1E5y5VpFHq9BINF5vifah-JK2SYQC4WDV54Ip5jSLTSjjeBdf3g9ROKXc"> LINK</a></p>
     <iframe id="csok-map-big" frameborder="0" src="https://www.google.com/maps/d/u/0/embed?mid=1qH4VJVp5xs6vaKHyEHwmKth3kFMj4AP9&ll=46.19367658681184%2C18.40386481697169&z=10" allowfullscreen ></iframe>
 </div>
 
