18885508801744ac8bce790bbcdfd59f912f2582
Csernus Zoltán <d3v3lop3r1@Csernuss-iMac.local>
2020-04-09 23:16:22 +0200

    cookie osano

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -10,4 +10,5 @@
 <link rel="stylesheet" type="text/css" href="/css/all.css">
 <link rel="stylesheet" type="text/css" href="/css/style.css?<?php echo time(); ?>">
 <link rel="stylesheet" type="text/css" href="/css/lightbox.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
 
