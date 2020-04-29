046246d15439a1c690ae20cb28d311ff78d48e5c
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-04-29 15:47:45 +0200

    photo controller max 10000

--- app/Http/Controllers/photoController.php
+++ app/Http/Controllers/photoController.php
@@ -86,7 +86,7 @@ class photoController extends Controller
             $input= $request->all();
             $file = $request->file('file');
             $rules = array(
-		    'file' => 'image|max:4000',
+		    'file' => 'image|max:10000',
             );
             $validation = Validator::make($input, $rules);
 
