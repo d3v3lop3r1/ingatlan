c768b2359ef1fa0455117468cd2b64fcc560e233
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-05-05 10:53:08 +0200

    app id to new view page

--- resources/views/pages/show_new.blade.php
+++ resources/views/pages/show_new.blade.php
@@ -72,7 +72,7 @@ $filter= config('property.filter.hu')
             </div>
         </section>
         <section id="ingatlanok-lista">
-            <div class="container   id="app"">
+            <div class="container" id="app">
                 <div class="row ingatlan-sor">
                     @foreach ($properties as $property)
                             <?php
