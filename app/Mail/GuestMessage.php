737302eb0b52dd1a9262e5ebdf29e6508e541740
Csernus Zoltán <d3v3lop3r1@Csernuss-iMac.local>
2020-05-14 12:25:34 +0200

    message for index page

---
+++ app/Mail/GuestMessage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class GuestMessage extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $guest;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($guest)
+    {
+        $this->guest = $guest;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('guest-message@foxinfo.hu')->view('emails.guest-message');
+    }
+}
