0358dcab5a1b5a5e84e9c9120b43c10b344fb61d
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-05-18 06:18:30 +0200

    messenger de and navbar style

--- resources/views/de_layouts/main.blade.php
+++ resources/views/de_layouts/main.blade.php
@@ -11,6 +11,7 @@
     @include('de_layouts.newnavbar')
     @include('de_layouts.carousel-main')
     @include('de_layouts.search')
+    @include('de_layouts.modals.messenger')
     @yield('tartalom')
     @include('de_layouts.footer')
 </body>
