3a6fc3eab6995c1d433cf364d8e0c78896819937
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-05-25 04:10:00 +0200

    delete import css from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
--- resources/views/scripts/script.blade.php
+++ resources/views/scripts/script.blade.php
@@ -1,9 +1,9 @@
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></script>
+<script src="/js/jquery-3.5.1.min.js" ></script>
 <script src="/js/app.js"></script>
 <script src="/js/bootstrap.js"></script>
 
-<script src="/js/lightbox-plus-jquery.js"></script>
+<script src="/js/lightbox.min.js"></script>
 <script src="/js/simple.money.format.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
