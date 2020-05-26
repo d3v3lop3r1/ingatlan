32c63cdaff5b539e5f3f5c9a174693d83e5d9015
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-05-26 11:02:53 +0200

    mentett ingatlanok scoped style

--- resources/js/components/ShowFavourites.vue
+++ resources/js/components/ShowFavourites.vue
@@ -3,7 +3,7 @@
     <div>
         <div v-if="!error">
             <div v-for="(property, index) in properties" v-bind:key="index" class="media">
-                <a :href="'/index/'+property.id"><img :src="imgLink" class="mr-3" alt="..."></a>
+                <!-- <a :href="'/index/'+property.id"><img :src="imgLink" class="mr-3" alt="..."></a> -->
                 <div class="media-body">
                     <a :href="'/index/'+property.id"><h6 class="mt-0">{{110000+property.id}} {{property.header_hun}}</h6></a>
                     <p v-html="property.short_text_hun"></p>  
@@ -55,4 +55,26 @@ export default {
     }    
 }
 </script>
+<style scoped>
+    .navbar-favourites-container img{
+        width: 44px;
+        height: 44px;
+    }
+
+    .navbar-favourites-container{
+        text-transform: none;
+        max-width: 70vw;
+        font-size: x-small;
+    }
+
+    .navbar-favourites-container .media{
+        padding: 0 10px;
+        margin-bottom: 5px;
+    }
+
+    .navbar-favourites-container p{
+        margin: 0 0 5px 0;
+    }
+</style>
+
 
