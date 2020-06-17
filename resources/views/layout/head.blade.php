44ec323be1aba2b1eedeb4be07ebda5be480c0dc
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-06-17 20:03:57 +0200

    pixel code embed

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -10,4 +10,20 @@
 <link rel="stylesheet" type="text/css" href="/css/lightbox.css">
 <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/css/style.css?<?php echo time(); ?>">
-
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '267248001265322');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=267248001265322&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
