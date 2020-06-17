96a4b9bd177559bfdf4689b7d9ea87b7b02e7c46
Csernus Zoltán <d3v3lop3r1@iMac.local>
2020-06-17 17:39:38 +0200

    fb webhook working

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -9,7 +9,8 @@ class FacebookController extends Controller
     public function messenger_verify(Request $request){
         $local_verify_token=env('WEBHOOK_VERIFY_TOKEN');
         $hub_verfify_token= $request->input('hub_verify_token');
-        if ($local_verify_token == $hub_verfify_token){
+        $hub_mode=$request->input('hub_mode');
+        if ($local_verify_token == $hub_verfify_token && $hub_mode == 'subscribe'){
             return $request->input('hub_challenge');
 
         } else {
