a1fbe435dac348339cdf24c272e24c088133e157
D3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-06-23 23:26:23 +0200

    new api map key

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -228,7 +228,7 @@ if ($property->act_price){
                         <div class="card-body">
                             <iframe
                             frameborder="0" style="border:0; width:100%; height:300px;"
-                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBrdH7bXJbDwNHSyPVwWIoyIeTdiUbWP_c
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAKV7YR9Rs30s1PfDlXiJ8ybbdS3iYMPTo
                             &q={{$property->city}}%2C%20{{$property->street}}" allowfullscreen >
                             </iframe>
                         </div>
