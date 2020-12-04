2ed8c4d1b17164e4b18beb37008ad4a6caedb81e
d3v3lop3r1 <d3v3lop3r1@gmail.com>
2020-12-04 13:04:24 +0100

    503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <title>Fejlesztés alatt</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <img class="img-fluid" src="./images/error/reactjs.jpg" alt="React Js fejlesztés">
+                <h3>Ingatlanfox.hu</h3>
+                <h5 class="text-center">fejlesztés alatt ...</h5>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
